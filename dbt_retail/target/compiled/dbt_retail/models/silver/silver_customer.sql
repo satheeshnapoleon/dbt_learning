102093726588d96cd7e7a2db9808ba0cbfeafc63
satheeshnapoleon <info.satheeshsubramaniam@gmail.com>
2026-01-29 10:50:04 +0530

    second commit

---
+++ dbt_retail/target/compiled/dbt_retail/models/silver/silver_customer.sql
@@ -0,0 +1,24 @@
+
+
+select 
+    try_cast(customer_code as varchar(30))        as customer_code,
+    try_cast(first_name as varchar(30))           as customer_first_name,
+    try_cast(last_name as varchar(30))            as customer_last_name,
+
+    try_cast(
+        
+    coalesce(first_name, '') || ' ' || coalesce(last_name, '')
+
+        as varchar(60)
+    )                                             as customer_full_name,
+
+    try_cast(gender as varchar(10))               as gender_type_of_customer,
+    try_cast(email as varchar(40))                as customer_email,
+
+    try_cast(phone as varchar(20))                as customer_mobile_number,
+
+    try_to_timestamp_ntz(signup_date)              as customer_signup_date,
+    ingest_ts
+
+from retails.bronze.stg_customer
+
